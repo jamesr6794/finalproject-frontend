2842145648d90d814d1619cf20f84f55cffaddd7
James Rodriguez <JamesR94@macbook-pro.myfiosgateway.com>
2021-04-02 11:30:16 -0400

    basic frontend up

---
+++ frontend-todo/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+import './App.css';
+
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default App;
