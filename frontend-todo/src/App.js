4d566c58b02da41031df336f5c5582dc4c851a25
James Rodriguez <JamesR94@macbook-pro.myfiosgateway.com>
2021-04-06 11:51:56 -0400

    call form in app

--- frontend-todo/src/App.js
+++ frontend-todo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import Form from "./components/Form"
 
 import './App.css';
 
@@ -7,10 +7,15 @@ import './App.css';
 function App() {
   const [todos, makeTodos] = useState([])
 
+  function createTodo(todo) {
+    makeTodos([todo, ...todos])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>My To-Do List</p>
+        <Form createTodo={createTodo} />
       </header>
     </div>
   )
